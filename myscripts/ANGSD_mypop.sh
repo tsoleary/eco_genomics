0cc5b8a0351ddb1c8fa87c71cd20b2dffdc71f21
tsoleary <tsoleary12@gmail.com>
2020-02-19 09:49:48 -0500

    added new parts to the script

--- myscripts/ANGSD_mypop.sh
+++ myscripts/ANGSD_mypop.sh
@@ -20,7 +20,7 @@ REF="/data/project_data/RS_ExomeSeq/ReferenceGenomes/Pabies1.0-genome_reduced.fa
 
 ANGSD -b ${output}/${mypop}_bam.list \
 -ref ${REF} -anc ${REF} \
--out ${output}/${mypop}_allsites \
+-out ${output}/${mypop}_folded_allsites \
 -nThreads 1 \
 -remove_bads 1 \
 -C 50 \
@@ -37,10 +37,10 @@ ANGSD -b ${output}/${mypop}_bam.list \
 -doMajorMinor 1 \
 -doMaf 1 \
 -doSaf 1 \
--doHWE 1 \
+-doHWE 1
 # -SNP_pval 1e-6
 
-# the -SNP line keeps the monomorphic sites but if you wanted only polymorphic sites then you would 
+# the -SNP line keeps the monomorphic sites but if you wanted only polymorphic sites then you would
 # -C downgrades reads that have really poor quality
 # -baq is a base quality score, when you have an indel you are morelikely to have snps because of misalignment so this discounts snps near indels
 # -minQ individual bases have to have a certain
@@ -51,3 +51,63 @@ ANGSD -b ${output}/${mypop}_bam.list \
 # skipTriallelic makes it so there are only dimorphic sites, it is likely that there are only two alternative alleles at each site
 # identifies doMajorMinor major and minor alleles for the SNPS it is ofen the case that the major allele is the ancestral allele and the minor allele is the derived alleles
 # doHWE estimates HW Equilibruim
+
+# ------------------------------------------------------------------------------
+
+# now we are going to do a folded spectrum that flips the minor and major allele frequencies because there are some sites where the reference genome may not have the real ancestral state for the genome
+
+# let's calculate the SFS floor the folded minor allele frequencies
+
+ANGSD -b ${output}/${mypop}_bam.list \
+-ref ${REF} -anc ${REF} \
+-out ${output}/${mypop}_folded_allsites \
+-nThreads 1 \
+-remove_bads 1 \
+-C 50 \
+-baq 1 \
+-minMapQ 20 \
+-minQ 20 \
+-setMinDepth 3 \
+-minInd 2 \
+-setMinDepthInd 1 \
+-setMaxDepthInd 17 \
+-skipTriallelic 1 \
+-GL 1 \
+-doCounts 1 \
+-doMajorMinor 1 \
+-doMaf 1 \
+-doSaf 1 \
+-fold 1
+
+
+# get a rough first estimate of the SFS and then use as a
+realSFS ${output}/${mypop}_folded_allsites.saf.idx
+-maxiter 1000 -tole 1e-6 -P 1 \
+>${output}/${mypop}_outFold.sfs
+
+
+ANGSD -b ${output}/${mypop}_bam.list \
+-ref ${REF} -anc ${REF} \
+-out ${output}/${mypop}_folded_allsites \
+-nThreads 1 \
+-remove_bads 1 \
+-C 50 \
+-baq 1 \
+-minMapQ 20 \
+-minQ 20 \
+-setMinDepth 3 \
+-minInd 2 \
+-setMinDepthInd 1 \
+-setMaxDepthInd 17 \
+-skipTriallelic 1 \
+-GL 1 \
+-doCounts 1 \
+-doMajorMinor 1 \
+-doMaf 1 \
+-doSaf 1 \
+-fold 1
+-pest ${output}/${mypop}_outFold.sfs \
+-doTheta 1
+# use the Theta output to estimate nuceotide diversity
+
+thetaStat do_stat ${output}/${mypop}_folded_allsites.thetas.idx
