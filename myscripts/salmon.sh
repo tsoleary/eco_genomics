f4e1f78248da200c1f3f1bc8858573d0ef800677
tsoleary <tsoleary12@gmail.com>
2020-03-04 09:35:28 -0500

    init

---
+++ myscripts/salmon.sh
@@ -0,0 +1,18 @@
+#! /bin/bash/
+
+# run salmon to do the mapping and the counting of the cleanreads
+
+# salmon quant -i transcripts_index -l <LIBTYPE> -r reads.fq --validateMappings -o transcripts_quant
+
+for file in /data/project_data/RS_RNASeq/fastq/cleanreads/CAM_02_H_*.cl.fq
+
+do
+
+  salmon quant \
+  -i /data/project_data/RS_RNASeq/ReferenceTranscriptome/Pabies_cds_index \
+  -l A \
+  -r ${file} \
+  --validateMappings \
+  -o /data/project_data/RS_RNASeq/salmon/cleanedreads
+
+done
