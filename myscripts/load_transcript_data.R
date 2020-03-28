5290ed27ee3bbeb72b06f095440b45039661fd2b
tsoleary <tsoleary12@gmail.com>
2020-03-27 21:35:25 -0400

    added factors and other deseq res

--- myscripts/load_transcript_data.R
+++ myscripts/load_transcript_data.R
@@ -14,12 +14,7 @@ counts <- round(read.table("RS_cds2kb_countsMatrix.txt", header = TRUE))
 
 # Load the metadata
 metadata <- read_delim("RS_samples.txt", delim = "\t",
-                       col_types = cols(
-                         sample = col_character(),
-                         pop = col_factor(),
-                         treatment = col_factor(),
-                         day = col_factor(),
-                         climate = col_factor()))
+                       col_types = "cffff")
 
 # Filter to only have the Day 10 samples 
 metadata <- metadata %>%
@@ -52,12 +47,67 @@ resultsNames(dds)
 res_climate <- results(dds, contrast = c("climate", "HD", "CW"), alpha = 0.05)
 res_D_C <- results(dds, contrast = c("treatment", "D", "C"), alpha = 0.05)
 res_H_C <- results(dds, contrast = c("treatment", "H", "C"), alpha = 0.05)
+#res_int <- results(dds, name = "climateCW.treatmentH")
+#res_int <- results(dds, name = "climateCW.treatmentD")
 
-summary(res_climate)
-summary(res_D_C)
-summary(res_H_C)
+# summary(res_climate)
+# summary(res_D_C)
+# summary(res_H_C)
 
-# splitting the groups up with paste and looking at it that way?....
+# Transform for PCA ------------------------------------------------------------
+vsd <- vst(dds)
+pc_data <- plotPCA(vsd, 
+                   ntop = 100000,
+                   intgroup = c("climate", "treatment"), 
+                   returnData = TRUE)
+percentVar <- round(100 * attr(pc_data, "percentVar"))
+
+# reordering the factors
+pc_data$treatment <- factor(pc_data$treatment, 
+                         levels = c("C", "H", "D"), 
+                         labels = c("C", "H", "D"))
+
+# Combining factors ------------------------------------------------------------
 metadata <- metadata %>%
   mutate(group = paste(climate, treatment, sep = "_"))
+
+dds <- DESeqDataSetFromMatrix(countData = counts,
+                              colData = metadata,
+                              design = ~ group)
+
+# Set the group to be compared against CW comparisons -----
+dds$group <- relevel(dds$group, ref = "CW_C")
+
+# Filter out genes with few reads
+dds <- dds[rowSums(counts(dds)) > 30]
+
+# Run DESeq
+dds <- DESeq(dds)
+
+# List the results you've generated
+resultsNames(dds)
+
+res_CW_D <- results(dds, contrast = c("group", "CW_D", "CW_C"), alpha = 0.05)
+res_CW_H <- results(dds, contrast = c("group", "CW_H", "CW_C"), alpha = 0.05)
+
+# summary(res_CW_D)
+# summary(res_CW_H)
+
+# Set the group to be compared against HD comparisons -----
+dds$group <- relevel(dds$group, ref = "HD_C")
+
+# Filter out genes with few reads
+dds <- dds[rowSums(counts(dds)) > 30]
+
+# Run DESeq
+dds <- DESeq(dds)
+
+# List the results you've generated
+resultsNames(dds)
+
+res_HD_D <- results(dds, contrast = c("group", "HD_D", "HD_C"), alpha = 0.05)
+res_HD_H <- results(dds, contrast = c("group", "HD_H", "HD_C"), alpha = 0.05)
+
+# summary(res_HD_D)
+# summary(res_HD_H)
 
