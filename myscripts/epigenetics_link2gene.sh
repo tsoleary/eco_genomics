50d8a4b16d4ca04489f13b90cacba7cec7326410
tsoleary <tsoleary12@gmail.com>
2020-04-08 09:43:28 -0400

    added paths

--- myscripts/epigenetics_link2gene.sh
+++ myscripts/epigenetics_link2gene.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/data/popgen/bedtools2/bin/bedtools closest -a ~/eco_genomics/diffmeth.bed \
+/data/popgen/bedtools2/bin/bedtools closest -a ~/eco_genomics/myresults/epigenetics/diffmeth.bed \
       -b /data/project_data/epigenetics/GCA_900241095.1_Aton1.0_genomic.fa_annotation_table.bed \
       -D b | \
-      awk '!($10=="-")' > hits.bed
+      awk '!($10=="-")' > ~/eco_genomics/myresults/epigenetics/hits.bed
 
 # the annotation file here has the format: ScaffoldName  FromPosition  ToPosition  Sense TranscriptName  TranscriptPath  GeneAccession GeneName  GeneAltNames  GenePfam  GeneGo  CellularComponent MolecularFunction BiologicalProcess
 
