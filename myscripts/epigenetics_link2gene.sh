bba9f0e8e9811addda0606241a03d74b5e24f11e
tsoleary <tsoleary12@gmail.com>
2020-04-08 10:55:44 -0400

    added the comps for the write up

--- myscripts/epigenetics_link2gene.sh
+++ myscripts/epigenetics_link2gene.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-/data/popgen/bedtools2/bin/bedtools closest -a ~/eco_genomics/myresults/epigenetics/diffmeth.bed \
+/data/popgen/bedtools2/bin/bedtools closest -a ~/eco_genomics/myresults/epigenetics/HA_diffmeth.bed \
       -b /data/project_data/epigenetics/GCA_900241095.1_Aton1.0_genomic.fa_annotation_table.bed \
       -D b | \
-      awk '!($10=="-")' > ~/eco_genomics/myresults/epigenetics/hits.bed
+      awk '!($10=="-")' > ~/eco_genomics/myresults/epigenetics/HA_hits.bed
+
+/data/popgen/bedtools2/bin/bedtools closest -a ~/eco_genomics/myresults/epigenetics/HH_diffmeth.bed \
+      -b /data/project_data/epigenetics/GCA_900241095.1_Aton1.0_genomic.fa_annotation_table.bed \
+      -D b | \
+      awk '!($10=="-")' > ~/eco_genomics/myresults/epigenetics/HH_hits.bed
 
 # the annotation file here has the format: ScaffoldName  FromPosition  ToPosition  Sense TranscriptName  TranscriptPath  GeneAccession GeneName  GeneAltNames  GenePfam  GeneGo  CellularComponent MolecularFunction BiologicalProcess
 
