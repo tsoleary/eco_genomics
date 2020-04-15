bc3c76c63ec99f26d48e847110c1aae8f2f3370c
tsoleary <tsoleary12@gmail.com>
2020-04-15 14:35:27 -0400

    res_ rm

--- myscripts/load_transcript_gwas_env_grp.R
+++ myscripts/load_transcript_gwas_env_grp.R
@@ -9,10 +9,10 @@ require(tidyverse)
 
 # List of files to load
 setwd(here::here("myresults/gwas_env"))
-res_files <- list.files()[str_which(list.files(), "res")]
-res_names <- str_replace(str_extract(res_files, "res.+"), ".rds", "")
+files <- list.files()[str_which(list.files(), "res")]
+names <- str_replace(str_extract(files, "res.+"), ".rds", "")
 
 # Read in .rds file and assign each object to the base name of the file
-for (i in seq_along(res_files)) {
-  assign(res_names[i], readRDS(res_files[i]))
+for (i in seq_along(files)) {
+  assign(names[i], readRDS(files[i]))
 }
